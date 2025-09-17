75b617d05e0ba332ab6056bdb122ecdc71b7d87d
Daniel25TE <danielalejandrosud25@gmail.com>
2025-09-17 09:21:18 -0600

    W03 Team Activity: Dynamic Header and Footer plus stretch activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { getParam  } from './utils.mjs';
+import { getParam, loadHeaderFooter  } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 import ProductDetails from './ProductDetails.mjs';
 
@@ -13,6 +13,7 @@ const dataSource = new ProductData('tents');
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
+loadHeaderFooter();
 
 
 /*function addProductToCart(product) {
