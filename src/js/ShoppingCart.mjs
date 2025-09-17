871ce7f0b9b230548be51f168c6f34cecd7cdf7c
Daniel25TE <danielalejandrosud25@gmail.com>
2025-09-17 15:26:37 -0600

    W03 Individual Task(s): Report - Total in cart

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -3,7 +3,7 @@ import { getLocalStorage, setLocalStorage, renderListWithTemplate, discount } fr
 function cartItemTemplate(item) {
   return `<li class="cart-card divider">
     <a href="#" class="cart-card__image">
-      <img src="${item.Image}" alt="${item.Name}" />
+      <img src="${item.Images.PrimaryMedium}" alt="${item.Name}" />
     </a>
     <a href="#">
       <h2 class="card__name">${item.Name}</h2>
@@ -16,21 +16,48 @@ function cartItemTemplate(item) {
     <button class="remove-cart-item cart-card__remove" id="${item.Id}" aria-label="Remove from cart">✕</button>
   </li>`;
 }
-
+//total cart logic in case we need to switch branches starts
+function getDiscountedNumber(item) {
+  const html = discount(item);                   
+  const match = html.match(/class="new-price">\$([\d.]+)/);
+  return match ? parseFloat(match[1]) : 0;
+}
+//total cart logic in case we need to switch branches ends
 export default class ShoppingCart {
   constructor(listElement) {
     this.listElement = listElement;
     this.cartItems = this.getCartItems();
+    //total cart logic in case we need to switch branches starts
+    this.cartFooter = document.querySelector('.cart-footer');
+    this.cartTotal = document.querySelector('.cart-total');
+    //total cart logic in case we need to switch branches ends
   }
 
   getCartItems() {
     return getLocalStorage('so-cart') || [];
   }
+  //total cart logic in case we need to switch branches starts
+  calculateTotal() {
+  return this.cartItems.reduce((sum, item) => sum + getDiscountedNumber(item), 0);
+}
 
-    renderCart() {
+  renderTotal() {
+    if (this.cartItems.length > 0) {
+      this.cartFooter.classList.remove('hide');
+      const total = this.calculateTotal().toFixed(2);
+      this.cartTotal.textContent = `Total: $${total}`;
+    } else {
+      this.cartFooter.classList.add('hide');
+    }
+  }
+  //total cart logic in case we need to switch branches ends
+  renderCart() {
     this.listElement.innerHTML = '';
     renderListWithTemplate(cartItemTemplate, this.listElement, this.cartItems);
     this.addRemoveButtons();
+    //total cart logic in case we need to switch branches starts
+    this.renderTotal();
+    //total cart logic in case we need to switch branches ends
   }
 
   addRemoveButtons() {
