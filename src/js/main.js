624b1a8baaad018e46ca5c92da740d0998d94ad7
Peter Mostert <peter@pdm-pix.com>
2025-09-17 20:15:37 +0200

    refactor: Comment out unused product list initialization code

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,3 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
 import { loadHeaderFooter } from './utils.mjs';
 
-const productData = new ProductData("tents");
-const listElement = document.querySelector('.product-list');
-
-async function initProductList() {
-  await productData.getData(); // Ensure data is loaded
-  const productList = new ProductList('Tents', productData, listElement);
-  productList.init();
-}
 loadHeaderFooter();
-initProductList();
